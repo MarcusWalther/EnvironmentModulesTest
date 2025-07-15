dc7ec1409c07c4bff7f8c5583dac378b8fe75fc8
marcus <walther72@hotmail.de>
2025-07-15 12:11:52 +0200

    * Added test for meta module creation * Fixed issue with latest Powershell versions and unset environment modules

--- Creation.Tests.ps1
+++ Creation.Tests.ps1
@@ -47,6 +47,7 @@ Describe 'TestModuleCreation' {
         $moduleInfo.Architecture | Should -BeExactly "x86"
         $moduleInfo.Parameters[[System.Tuple[string, string]]::new("Param1", "Default")].Value | Should -BeExactly "Param1Value"
         $moduleInfo.Parameters.Length | Should -BeExactly 1
+        $moduleInfo.ModuleType | Should -BeExactly "Default"
     }
 
     It 'Module Creation Multiple Dependencies' {
@@ -54,5 +55,11 @@ Describe 'TestModuleCreation' {
         $moduleInfo = New-EnvironmentModuleInfo -ModuleFile "$moduleDirectory/TestModule-4.5-x86.psd1"
         $moduleInfo.Dependencies[0].ModuleFullName | Should -BeExactly "ModuleDependency"
         $moduleInfo.Dependencies[1].ModuleFullName | Should -BeExactly "ModuleDependency2"
+    }
+
+    It 'Module Creation Meta' {
+        New-EnvironmentModule -Name "TestModule" -Author "Max Mustermann" -Description "My Test Module" -Version "4.5" -Architecture "x86" -RequiredFiles "temp.exe" -SearchPaths "C:\temp" -Path "$global:tempDirectory" -ModuleType "Meta" -Force
+        $moduleInfo = New-EnvironmentModuleInfo -ModuleFile "$moduleDirectory/TestModule-4.5-x86.psd1"
+        $moduleInfo.ModuleType | Should -BeExactly "Meta"
     }
 }
